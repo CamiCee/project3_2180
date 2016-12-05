441961411c4f99cba407e9d59ce73f741a9704fa
Camille Campbell <c.campbell1996@hotmail.com>
2016-12-05 03:30:36 +0000

    Allows the user to logout

---
+++ intermediate.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <!--<script src="login.js" type="text/javascript"></script>-->
+    </head>
+        
+    <body>
+        <form action="logout.php" >
+			<button id="submitBtn">Logout</button>
+        </form>
+    </body>
+</html>
